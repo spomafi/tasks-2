a27da7b6e6531d589ca4366b677b59c3b07a78c2
Mariya Sharova <marii.savit@gmail.com>
2023-04-19 18:21:00 +0300

    fix code

--- task3/index.js
+++ task3/index.js
@@ -6,7 +6,7 @@ function numToPr(number){
     o = ['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'],
     o2 = ['одна', 'две'],
     p = ['одиннацать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'],
-    th = ['тысяча', 'тысяч', 'тысячи'];
+    th = ['тысяча', 'тысячи', 'тысяч'];
 
   let str = number.toString(), out = '';
 
@@ -15,23 +15,64 @@ function numToPr(number){
       return z[0]
     }
     return o[number - 1];
-  }
-  else if (str.length === 2) {
-    if (str[0] === 1) out = p[parseInt(str[1]) - 1];
-    else out = (t[parseInt(str[0]) - 1] + ((str[1] !== '0') ? (' ' + o[parseInt(str[1]) - 1]) : ''));
-  }
-  else if (str.length === 3) {
-    out = (h[parseInt(str[0]) - 1] + ((str[1] !== '0') ? (' ' + t[parseInt(str[1]) - 1]) : '') + ((str[2] !== '0') ? (' ' + o[parseInt(str[2]) - 1]) : ''));
-  }
-  // else if (str.length === 4) {}
-  // else if (str.length === 5) {}
-  else if (number === 100000) {
-    out = (h[0] + ' ' + th[1])
   } 
-
-  let arr = out.split('');
-  out = arr.join('');
+  else if (str.length === 2) {
+    if (parseInt(str[0]) === 1 && parseInt(str[1]) !== 0) out = p[parseInt(str[1]) - 1];
+    else out = (str[0] !== "0" ? " " + t[parseInt(str[0]) - 1] : "") + (str[1] !== "0" ? " " + o[parseInt(str[1]) - 1] : "");
+  } 
+  else if (str.length === 3) {
+    out = h[parseInt(str[0]) - 1];
+    if (parseInt(str[1]) === 1) out += " " + p[parseInt(str[2]) - 1];
+    else out += (str[1] !== "0" ? " " + t[parseInt(str[1]) - 1] : "") + (str[2] !== "0" ? " " + o[parseInt(str[2]) - 1] : "");
+  } 
+  else if (str.length === 4) {
+    if (parseInt(str[0]) < 3) {
+        out += o2[parseInt(str[0]) - 1];
+      if (parseInt(str[0]) === 1) {
+        out += " " + th[0];
+      } else {
+        out += " " + th[1];
+      }
+    } 
+    else if (parseInt(str[0]) < 5) {
+      out += o[parseInt(str[0]) - 1] + " " + th[1];
+      } else {
+      out += o[parseInt(str[0]) - 1] + " " + th[2];
+    }
+    out += str[1] !== "0" ? " " + h[parseInt(str[1]) - 1] : "";
+    if (parseInt(str[2]) === 1) out += " " + p[parseInt(str[3]) - 1];
+    else out += " " + t[parseInt(str[2]) - 1] + (str[3] !== "0" ? " " + o[parseInt(str[3]) - 1] : "");
+  } 
+  else if (str.length === 5) {
+    if (parseInt(str[0]) === 1) {
+      out += " " + p[parseInt(str[1]) - 1] + " " + th[2];
+      } else {
+        out += " " + t[parseInt(str[0]) - 1];
+      if (parseInt(str[1]) < 3) {
+          out += " " + o2[parseInt(str[1]) - 1];
+        if (parseInt(str[1]) === 1) {
+          out += " " + th[0];
+        } else {
+          out += " " + th[1];
+        }
+    } 
+    else if (parseInt(str[1]) < 5) {
+        out += " " + o[parseInt(str[1]) - 1] + " " + th[1];
+      } else {
+        out += " " + o[parseInt(str[1]) - 1] + " " + th[2];
+    }
+    }
+    out += str[2] !== "0" ? " " + h[parseInt(str[2]) - 1] : "";
+    if (parseInt(str[3]) === 1) out += " " + p[parseInt(str[4]) - 1];
+    else out += " " + t[parseInt(str[3]) - 1] + (str[4] !== "0" ? " " + o[parseInt(str[4]) - 1] : "");
+  }
+  else if (str.length === 6) {
+    out = h[parseInt(str[0]) - 1] + (str[1] !== "0") ? " " + th[parseInt(str[3]) - 1] : "";
+  }
+    
+  let arr = out.split("");
+  out = arr.join("");
   return out;
 }
-
-console.log(numToPr(100000));
+    
+console.log(numToPr(191));
